745e62f08a9f66e4245fc960c5776d4e6c557b8c
Yuko <yuko.mazurek@gmail.com>
2019-02-22 18:25:47 -0800

    Updates

---
+++ precache-manifest.56f18a1590d1b2ee278fbad9ab426c43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/art-museum/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d7fb52c13cb1ff7b7b9c",
+    "url": "/art-museum/static/js/runtime~main.d7fb52c1.js"
+  },
+  {
+    "revision": "953a2d814d658cd98d06",
+    "url": "/art-museum/static/js/main.953a2d81.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/art-museum/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "953a2d814d658cd98d06",
+    "url": "/art-museum/static/css/main.48927e74.chunk.css"
+  },
+  {
+    "revision": "2b9a41c8d43c9c248930726782699a83",
+    "url": "/art-museum/index.html"
+  }
+];
